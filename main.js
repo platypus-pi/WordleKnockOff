f9c69401aa8820028787c376b71b61e455df50eb
Daphney552632 <dt2007139@gmail.com>
2022-03-25 16:09:43 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
+//debuggy this
 var n = 5;
 var theBoard;
 function initGame(){
     theBoard = new Board(n,n,"arise");
     document.getElementById("spotForBoard").appendChild(theBoard.elem);
 }
-document.addEventListener("keyup",function(event){
+document.addEventListener("keyup",function(e){
     const alphabet = "abcdefghijklmnopqrstuvwxyz";
-    var val = event.key; //string val
-    if(val=="Enter"){
+    var val = e.key; //string val
+    if(val=="Space"){
         theBoard.submit();
         document.write("you pressed space");
     }else if(val.length==1){
