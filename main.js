34de33109919b779eb8c0facfc566702b99c34b5
Daphney552632 <dt2007139@gmail.com>
2022-03-25 15:47:17 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 var n = 5;
-var theBoard = new Board(n,n,"arise");
+var theBoard;
+function initGame(){
+    theBoard = new Board(n,n,"arise");
+    document.getElementById("spotForBoard").appendChild(theBoard.elem);
+}
+document.addEventListener("keyup",function(event){
+    var val = event.keyCode;
+    alphabet = "abcdefghijklmnopqrstuvwxyz";
+    if(val==13){
+        theBoard.submit();
+    }else if(65<=val && val<65+26){
+        theBoard.typeIn(alphabet[val-65]);
+    }
+
+});
